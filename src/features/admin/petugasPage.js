b8f74f0f6b8df6657e9b1902326f733e5c2a19ec
Fadiah Faradinah Nasir <222112030@stis.ac.id>
2026-06-23 10:18:37 +0800

    perbaikan tambah petugas

--- src/features/admin/petugasPage.js
+++ src/features/admin/petugasPage.js
@@ -356,8 +356,6 @@ const PetugasPage = (() => {
     }
 
     btn.disabled = false;
-    document.getElementById('btn-pt-submit-label').textContent =
-      _editId ? 'Simpan Perubahan' : 'Tambah Petugas';
     btn.innerHTML = `
       <svg width="15" height="15" viewBox="0 0 24 24" fill="none" stroke="currentColor"
            stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round">
